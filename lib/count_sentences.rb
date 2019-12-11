092006aafea7a691573b404b2858f338f466c1da
Paul Leugemors <pleugemors@yahoo.com>
2019-12-11 00:59:31 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,32 @@ class String
 
   def count_sentences
     sentence = self.split(".")new
-    sentence.each do |sentence| 
-      
+    arr = []
+    arr2 = []
+    sentence.each do |sentence1|
+      if sentence1.include?("?")
+        arr << sentence1.split("?")
+      else
+        arr << sentence1
+      end
+    end
+    
+    arr.each do |sentence1|
+      if sentence1.include?("!")
+        arr2 << sentence1.split("!")
+      else
+        arr2 << sentence1
+      end
+    end
+    
+    sum = 0 
+    arr2.each do |element|
+      if element.size > 0 
+        sum += 1 
+      end
+    end
+     
+    sum
     end
   end
 end
